7fb44fcecc9b762a2f383e81dc155d1ab2a806ff
katand <katandps@gmail.com>
2020-08-31 00:38:55 +0900

    [feature] 起動用スクリプトを修正

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_serv
 
 $(aws ecr get-login --no-include-email)
 docker pull $IMAGE
-docker run -it -v $(pwd)/files:/app/files -v $(pwd)/config.toml:/app/config.toml -p 80:8000 $IMAGE
+docker run -itd -v $(pwd)/files:/app/files -v $(pwd)/config.toml:/app/config.toml -p 80:8000 $IMAGE
