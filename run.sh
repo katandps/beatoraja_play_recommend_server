f78d0ade1e9000105aaf88ad64f3f1468a9dc9de
katand <katandps@gmail.com>
2020-08-31 07:50:23 +0900

    [bug-fix] デプロイスクリプトを修正

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_serv
 
 $(aws ecr get-login --no-include-email)
 docker pull $IMAGE
+docker ps | awk '{print $1}' | xargs docker kill
 docker run -itd -v $(pwd)/files:/app/files -v $(pwd)/config.toml:/app/config.toml -p 80:8000 $IMAGE
