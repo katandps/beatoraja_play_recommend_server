775e54770598cd593f0ebb0bb02395198abdb3db
katand <katandps@gmail.com>
2020-10-04 20:07:23 +0900

    起動スクリプトで起動前にスコアデータをダウンロードするようにした

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_server
 
+rm files/*.db
+aws s3 sync s3://beatoraja-play-recommend files/
+
 $(aws ecr get-login --no-include-email)
 docker pull $IMAGE
 docker ps | awk '{print $1}' | xargs docker kill
