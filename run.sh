74be49781b9761cffafd698de0c18301c960de0d
katand <katandps@gmail.com>
2020-10-07 21:11:22 +0900

    デプロイスクリプトを、filesディレクトリがない場合にエラーにならないように修正

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_server
 
+mkdir -p files
 rm files/*.db
 aws s3 sync s3://beatoraja-play-recommend files/
 
