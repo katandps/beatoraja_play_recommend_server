bf7ee05a9a817c6e6a183d7c2d016c5db0b43899
katand <katandps@gmail.com>
2020-10-07 21:14:15 +0900

    デプロイスクリプトを、filesディレクトリがない場合にエラーにならないように修正

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_server
 
 mkdir -p files
-rm files/*.db
+rm -f files/*.db
 aws s3 sync s3://beatoraja-play-recommend files/
 
 $(aws ecr get-login --no-include-email)
