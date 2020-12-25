5eb4c97861df7c65ba4256ce1ca4349a3305b52c
katand <katandps@gmail.com>
2020-12-26 03:52:54 +0900

    run.shでのs3アクセスを削除

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 IMAGE=847110695266.dkr.ecr.ap-northeast-1.amazonaws.com/beatoraja_recommend_server
-
 mkdir -p files
-rm -f files/*.db
-aws s3 sync s3://beatoraja-play-recommend files/
 
 $(aws ecr get-login --no-include-email)
 docker-compose down
